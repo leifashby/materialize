5162840f63fb23e9af83cf7c90a9c7f2856d25f3
Leif Ashby <leif.ashby@gmail.com>
2017-02-10 15:56:17 -0500

    Adding check for jQuery object on window to prevent overloading.

--- js/initial.js
+++ js/initial.js
@@ -1,8 +1,12 @@
 // Check for jQuery.
 if (typeof(jQuery) === 'undefined') {
   var jQuery;
+  // jQuery global can be hidden, but it's in the window object. Check to prevent overloading.
+  if (typeof(window.jQuery) !== 'undefined') {
+    jQuery = $ = window.jQuery;
+  }
   // Check if require is a defined function.
-  if (typeof(require) === 'function') {
+  else if (typeof(require) === 'function') {
     jQuery = $ = require('jquery');
   // Else use the dollar sign alias.
   } else {
